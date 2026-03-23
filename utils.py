96e97eee972933d48b6cee5dc521acb7424a9aff
Grom <egor.maksimovich.s@gmail.com>
2026-03-23 13:08:24 +0300

    Добавлена универсальная функция для создания Embed'ов по переданным параметрам modified:   utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 import logging
+from typing import List, Any, Dict
 import asyncpg
+import discord
 from dotenv import load_dotenv
 
 # Инициализируем логгер для этого модуля
-logger = logging.getLogger('slashy.utils')
+logger = logging.getLogger("slashy.utils")
 # Загружаем переменные окружения из .env файла
 load_dotenv()
 
@@ -24,8 +26,7 @@ def get_env(key: str) -> str:
     value = os.getenv(key)
     if not value:
         logger.error(f"Переменная {key} не задана в .env")
-        raise ValueError(
-            f"Переменная {key} не задана в .env")
+        raise ValueError(f"Переменная {key} не задана в .env")
     return value
 
 
@@ -67,35 +68,107 @@ async def add_xp(user_id: int, guild_id: int, xp: int, pool: asyncpg.Pool):
     try:
         async with pool.acquire() as connection:
             # Начисляем опыт и получаем обновлённые данные о пользователе
-            row = await connection.fetchrow("""
+            row = await connection.fetchrow(
+                """
                 INSERT INTO user_levels (guild_id, user_id, xp)
                 VALUES ($1, $2, $3)
                 ON CONFLICT (guild_id, user_id) DO UPDATE
                 SET xp = user_levels.xp + EXCLUDED.xp
                 RETURNING xp, level
-            """, guild_id, user_id, xp)
+            """,
+                guild_id,
+                user_id,
+                xp,
+            )
             # Получаем данные о текущем опыте и уровне пользователя
-            curr_xp = row['xp']
-            curr_level = row['level']
+            curr_xp = row["xp"]
+            curr_level = row["level"]
             # Вычисляем требуемый опыт для следующего уровня
-            required_xp = 80*curr_level + 20*curr_level**2
+            required_xp = 80 * curr_level + 20 * curr_level**2
             # Поднимаем уровень пользователя, если опыта достаточно
             level_up = False
             while curr_xp >= required_xp:
                 # Поднимаем уровень пользователя, отнимая требуемый опыт
                 curr_xp -= required_xp
                 curr_level += 1
-                required_xp = 80*curr_level + 20*curr_level**2
+                required_xp = 80 * curr_level + 20 * curr_level**2
                 level_up = True
             # Обновляем уровень и оставшийся опыт в базе данных, если уровень был повышен
             if level_up:
-                await connection.execute("""
+                await connection.execute(
+                    """
                     UPDATE user_levels
                     SET level = $1, xp = $2
                     WHERE guild_id = $3 AND user_id = $4
-                """, curr_level, curr_xp, guild_id, user_id)
+                """,
+                    curr_level,
+                    curr_xp,
+                    guild_id,
+                    user_id,
+                )
         logger.info(
-            f"Начислено {xp} опыт пользователю {user_id} на сервере {guild_id}, текущий уровень: {curr_level}, текущий опыт: {curr_xp}")
+            f"Начислено {xp} опыт пользователю {user_id} на сервере {guild_id}, текущий уровень: {curr_level}, текущий опыт: {curr_xp}"
+        )
     except Exception as e:
         logger.error(f"Ошибка при начислении опыта: {e}", exc_info=True)
         raise e
+
+
+def create_embed(
+    title: str | None = None,
+    description: str | None = None,
+    color: discord.Color = discord.Color.blurple(),
+    thumbnail_url: str | None = None,
+    image_url: str | None = None,
+    footer_text: str | None = None,
+    footer_icon: str | None = None,
+    author_name: str | None = None,
+    author_icon: str | None = None,
+    fields: List[Dict[str, Any]] | None = None,
+    timestamp: bool = False,
+) -> discord.Embed:
+    """Универсальная функция для создания Embed
+
+    Args:
+        title (str | None, optional): Заголовок Embed. По умолчанию имеет значение None.
+        description (str | None, optional): Описание содержимого. По умолчанию имеет значение None.
+        color (discord.Color, optional): Цвет Embed. По умолчанию имеет значение discord.Color.blurple().
+        thumbnail_url (str | None, optional): URL компактной миниатюры. По умолчанию имеет значение None.
+        image_url (str | None, optional): URL изображения Embed. По умолчанию имеет значение None.
+        footer_text (str | None, optional): Текст нижней части Embed. По умолчанию имеет значение None.
+        footer_icon (str | None, optional): URL иконки нижней части Embed. По умолчанию имеет значение None.
+        author_name (str | None, optional): Имя автора. По умолчанию имеет значение None.
+        author_icon (str | None, optional): URL иконки автора. По умолчанию имеет значение None.
+        fields (List[Dict[str, Any]] | None, optional): Поля с содержимым в формате {"name": "...", "value": "...", "inline": True | False}. Defaults to None.
+        timestamp (bool, optional): Временная метка (если имеет значение True, в Embed будет добавлено текущее время). Defaults to False.
+
+    Returns:
+        discord.Embed: Созданный Embed с заданными параметрами
+    """
+
+    embed = discord.Embed(title=title, description=description, color=color)
+
+    if timestamp:
+        embed.timestamp = discord.utils.utcnow()
+
+    if thumbnail_url:
+        embed.set_thumbnail(url=thumbnail_url)
+
+    if image_url:
+        embed.set_image(url=image_url)
+
+    if footer_text:
+        embed.set_footer(text=footer_text, icon_url=footer_icon)
+
+    if author_name:
+        embed.set_author(name=author_name, icon_url=author_icon)
+
+    if fields:
+        for field in fields:
+            embed.add_field(
+                name=field.get("name", "..."),
+                value=field.get("value", "..."),
+                inline=field.get("inline", True),
+            )
+
+    return embed
