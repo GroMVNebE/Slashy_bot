5bee510511a207df71c161cf5e2889eca29168a6
Grom <egor.maksimovich.s@gmail.com>
2026-03-23 13:48:14 +0300

    Изменения в команде /lvl: - Аватар теперь получается асинхронно - Теперь аватар пользователя и заполненная карточка сохраняются в оперативной памяти с использованием io.BytesIO - Ограничена частота использования команды одним пользователем: 1 раз в 5 секунд - Сообщения об ошибках теперь более закрыты для пользователя

--- cogs/general.py
+++ cogs/general.py
@@ -1,31 +1,31 @@
+import aiohttp
 import discord
 import logging
-import os
+import io
 from PIL import Image, ImageDraw, ImageFont
 from discord import app_commands
 from discord.ext import commands
 from typing import TYPE_CHECKING
+from utils import create_embed
 
 if TYPE_CHECKING:
     from launch import Bot
 
 # Инициализируем логгер для этого модуля
-logger = logging.getLogger('slashy.general')
+logger = logging.getLogger("slashy.general")
 
 
 class General(commands.Cog):
     """Модуль с общими командами"""
 
-    def __init__(self, bot: 'Bot'):
+    def __init__(self, bot: "Bot"):
         self.bot = bot
         self.exp_font = ImageFont.truetype("assets/the_weekend.otf", 24)
         self.lvl_font = ImageFont.truetype("assets/the_weekend.otf", 126)
         self.username_font = ImageFont.truetype("assets/the_weekend.otf", 36)
 
-    @app_commands.command(
-        name="lvl",
-        description="Выводит Ваш текущий уровень"
-    )
+    @app_commands.command(name="lvl", description="Выводит Ваш текущий уровень")
+    @app_commands.checks.cooldown(1, 5.0, key=lambda i: (i.guild_id, i.user.id))
     async def lvl(self, interaction: discord.Interaction):
         """Выводит уровень и опыт пользователя, оформленный в embed-изображении
 
@@ -35,8 +35,12 @@ class General(commands.Cog):
         # Проверяем, что пул соединений с базой данных инициализирован
         if not self.bot.db_pool:
             logger.warning(
-                "Пул соединений с базой данных не инициализирован. Пропуск обработки сообщения")
-            await interaction.response.send_message("Произошла ошибка.. Пожалуйста, попробуйте позже", ephemeral=True)
+                "Пул соединений с базой данных не инициализирован. Пропуск обработки сообщения"
+            )
+            await interaction.response.send_message(
+                "Произошла ошибка при создании карточки с Вашим уровнем. Пожалуйста, попробуйте позже",
+                ephemeral=True,
+            )
             return
         # Получаем данные пользователя
         user = interaction.user
@@ -45,33 +49,61 @@ class General(commands.Cog):
         # Получаем данные об уровне и опыте пользователя из базы данных
         try:
             async with self.bot.db_pool.acquire() as connection:
-                row = await connection.fetchrow("""
+                row = await connection.fetchrow(
+                    """
                     SELECT xp, level FROM user_levels
                     WHERE guild_id = $1 AND user_id = $2
-                """, interaction.guild_id, user.id)
+                """,
+                    interaction.guild_id,
+                    user.id,
+                )
                 if not row:
                     xp, lvl = 0, 1
-                    await connection.execute("""
+                    await connection.execute(
+                        """
                         INSERT INTO user_levels (guild_id, user_id, xp, level)
                         VALUES ($1, $2, $3, $4)
-                    """, interaction.guild_id, user.id, xp, lvl)
+                    """,
+                        interaction.guild_id,
+                        user.id,
+                        xp,
+                        lvl,
+                    )
                 else:
-                    xp, lvl = row['xp'], row['level']
+                    xp, lvl = row["xp"], row["level"]
         except Exception as e:
             logger.error(
-                f"Ошибка при получении данных об уровне из БД: {e}", exc_info=True)
-            await interaction.followup.send("Произошла ошибка при получении данных о вашем уровне. Пожалуйста, попробуйте позже")
+                f"Ошибка при получении данных об уровне из БД: {e}", exc_info=True
+            )
+            await interaction.followup.send(
+                "Произошла ошибка при создании карточки с Вашим уровнем. Пожалуйста, попробуйте позже"
+            )
             return
         # Вычисляем прогресс до следующего уровня
-        required_xp = 80*lvl + 20*lvl**2
+        required_xp = 80 * lvl + 20 * lvl**2
         lvl_up_progress = xp / required_xp if required_xp > 0 else 0
         lvl_up_progress = min(lvl_up_progress, 1)  # Ограничиваем от 0 до 1
         try:
             # Загружаем шаблон карточки уровня
             bg = Image.open("assets/lvl_templ.png")
             # Получаем аватар пользователя, изменяем размер и вставляем на шаблон
-            await user.display_avatar.save('ava.png')
-            ava = Image.open('ava.png')
+            url = user.display_avatar.url
+            ava = None
+            ava_buff = None
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        data = await response.read()
+                        ava_buff = io.BytesIO(data)
+                        ava = Image.open(ava_buff).convert("RGBA")
+            if ava is None or ava_buff is None:
+                logger.error(
+                    f"Не удалось загрузить аватар пользователя {user.id}", exc_info=True
+                )
+                await interaction.followup.send(
+                    "Произошла ошибка при создании карточки с Вашим уровнем. Пожалуйста, попробуйте позже"
+                )
+                return
             ava = ava.resize(size=[65, 65])
             bg.paste(ava, (90, 138))
             # Получаем имя пользователя и обрезаем его, если оно слишком длинное для отображения на карточке
@@ -83,28 +115,67 @@ class General(commands.Cog):
             # Наачинаем изменение шаблона
             draw = ImageDraw.Draw(bg)
             # Вставляем на карточку
-            draw.text((167, 170), name, font=self.username_font,
-                      fill="#aed581", anchor="lm")
+            draw.text(
+                (167, 170), name, font=self.username_font, fill="#aed581", anchor="lm"
+            )
             # Вставляем уровень
-            draw.text((194, 325), str(lvl), font=self.lvl_font,
-                      fill="#aed581", anchor="mm")
+            draw.text(
+                (194, 325), str(lvl), font=self.lvl_font, fill="#aed581", anchor="mm"
+            )
             # Создаём круглый прогресс-бар, убирая лишнюю часть круга из шаблона
-            draw.arc((335, 217, 530, 412), start=-90+int(360 *
-                     lvl_up_progress), end=270, fill="#1d262a", width=36)
+            draw.arc(
+                (335, 217, 530, 412),
+                start=-90 + int(360 * lvl_up_progress),
+                end=270,
+                fill="#1d262a",
+                width=36,
+            )
             # Вставляет опыт
-            draw.text((432, 316), f"{xp}/{required_xp}",
-                      font=self.exp_font, fill="#aed581", anchor="mm")
-            # Сохраняем получившуюся карточку и отправляем пользователю
-            bg.save('assets/lvl_card.png')
-            card = discord.File('assets/lvl_card.png', filename='lvl_card.png')
-            await interaction.followup.send(file=card)
-            os.remove('ava.png')
-            os.remove('assets/lvl_card.png')
+            draw.text(
+                (432, 316),
+                f"{xp}/{required_xp}",
+                font=self.exp_font,
+                fill="#aed581",
+                anchor="mm",
+            )
+            # Сохраняем получившуюся карточку
+            buff = io.BytesIO()
+            bg.save(buff, format="PNG")
+            buff.seek(0)
+            card = discord.File(buff, filename=f"lvl_card_{user.id}.png")
+            # Добавляем карточку в Embed
+            embed = create_embed(
+                title=f"Данные об уровне {user.display_name}",
+                image_url=f"attachment://lvl_card_{user.id}.png",
+            )
+            # Отправляем Embed с карточкой пользователю
+            await interaction.followup.send(embed=embed, file=card)
+            # Закрываем буфер после отправки, чтобы освободить память
+            buff.close()
+            ava_buff.close()
         except Exception as e:
-            logger.error(
-                f"Ошибка при генерации карточки уровня: {e}", exc_info=True)
-            await interaction.followup.send("Произошла ошибка при генерации карточки Вашего уровня. Пожалуйста, попробуйте позже")
+            logger.error(f"Ошибка при генерации карточки уровня: {e}", exc_info=True)
+            await interaction.followup.send(
+                "Произошла ошибка при создании карточки с Вашим уровнем. Пожалуйста, попробуйте позже"
+            )
             return
+
+    @lvl.error
+    async def lvl_error(
+        self, interaction: discord.Interaction, error: app_commands.AppCommandError
+    ):
+        if isinstance(error, app_commands.CommandOnCooldown):
+            # Создаём embed с информацией о том, сколько осталось ждать до повторного использования команды
+            embed = create_embed(
+                title="Подождите перед повторным использованием команды",
+                description=f"Подождите ещё **{error.retry_after:.1f} сек.** перед повторным использованием команды",
+                color=discord.Color.red(),
+            )
+            # Отправляем пользователю сообщение
+            await interaction.response.send_message(
+                embed=embed,
+                ephemeral=True,
+            )
 
 
 async def setup(bot):
